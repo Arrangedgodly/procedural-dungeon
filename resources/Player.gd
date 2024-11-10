ab03090f8a42aab47bea9c5de1b4428d2e1fc8b7
Grady <arrangedgodly@gmail.com>
2024-11-10 13:35:44 -0700

    more sprite additions but no new code really

--- resources/Player.gd
+++ resources/Player.gd
@@ -1,2 +1,9 @@
 extends CharacterBody2D
 class_name Player
+
+@export var health: int
+@export var speed: int
+@export var basic_damage: int
+
+@export var sprite: AnimatedSprite2D
+@export var collision_shape: CollisionShape2D
